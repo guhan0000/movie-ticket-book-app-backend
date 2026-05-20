80905cdb96fe27079892b9116a96219c47dab5b8
guhan0000 <guhanm0000@gmail.com>
2026-05-20 22:00:07 +0530

    exception handler added

---
+++ demo/src/main/java/com/moviebookapp/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.moviebookapp.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(
+            RuntimeException ex) {
+
+        return new ResponseEntity<>(
+                Map.of("error", ex.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
