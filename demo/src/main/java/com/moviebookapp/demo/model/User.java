79e9c26feadbd72163abf7a7673bc8e37593b490
guhan0000 <guhanm0000@gmail.com>
2026-05-22 00:18:34 +0530

    user model added

--- demo/src/main/java/com/moviebookapp/demo/model/User.java
+++ demo/src/main/java/com/moviebookapp/demo/model/User.java
@@ -1,4 +1,21 @@
 package com.moviebookapp.demo.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long userId;
+    private String custName;
+    @Column(unique = true)
+    @Email
+    private String email;
+    private String password;
+    private String role;
+
 }
