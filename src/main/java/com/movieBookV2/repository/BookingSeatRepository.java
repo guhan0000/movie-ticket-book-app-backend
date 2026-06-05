7b3d5e771c04dcd877d015d18af3ec2be6a1a266
guhan0000 <guhanm0000@gmail.com>
2026-06-05 22:46:52 +0530

    repos added

---
+++ src/main/java/com/movieBookV2/repository/BookingSeatRepository.java
@@ -0,0 +1,11 @@
+package com.movieBookV2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.movieBookV2.model.BookingSeat;
+
+@Repository
+public interface BookingSeatRepository  extends JpaRepository<BookingSeat, Long>{
+
+}
---
+++ src/main/java/com/movieBookV2/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.movieBookV2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.movieBookV2.model.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
+	
+
+}
